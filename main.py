bb34fa7ae5a0a12420e901eaf8e65d0008f7b81e
Chirayu Subedi <chirayu.subedi.cs84@gmail.com>
2024-05-14 22:31:26 +0200

    Initial Committed

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import requests
+
+
+MY_API = "https://api.npoint.io/a3cb1b83a226cfa33970"
+# USE YOUR OWN point LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get(MY_API).json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
